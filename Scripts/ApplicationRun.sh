18dcb8690036597044854d59aca023ed565df03c
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-08-17 20:41:51 +0100

    .service file is not copied to systemd folder

--- Scripts/ApplicationRun.sh
+++ Scripts/ApplicationRun.sh
@@ -3,4 +3,5 @@ cd /home/ec2-user/ImageColourSwap
 export PATH=/home/ec2-user/.dotnet:$PATH
 export DOTNET_CLI_HOME=/tmp/
 
+cp ImageColourSwap.service /etc/systemd/system/
 sudo systemctl start ImageColourSwap.service
