02a27d0e680942a0d003200fcbfdcb88d1ecaf3b
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-10-17 20:29:42 +0100

    Edited afterinstall script.

--- Scripts/AfterInstall.sh
+++ Scripts/AfterInstall.sh
@@ -5,9 +5,12 @@ touch output.txt
 sudo wget https://dot.net/v1/dotnet-install.sh >> output.txt
 sudo chmod +x ./dotnet-install.sh >> output.txt
 sudo ./dotnet-install.sh >> output.txt
-
-dotnet tool install jsonsettingsupdater
-dotnet new tool-manifest
-dotnet nuget add source -n CommonRepo https://mk-267855555195.d.codeartifact.eu-west-2.amazonaws.com/nuget/Common/v3/index.json
 sudo yum install dotnet-sdk-6.0 -y
+
+export PATH=/home/ec2-user/.dotnet:$PATH
+export DOTNET_CLI_HOME=/tmp/
+
+dotnet nuget add source -n CommonRepo https://mk-267855555195.d.codeartifact.eu-west-2.amazonaws.com/nuget/Common/v3/index.json
+dotnet new tool-manifest
+dotnet tool install jsonsettingsupdater
 dotnet tool run jsonupdate ImageColourSwap/appsettings.json Integration ProcessingUri
