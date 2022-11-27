c0447422c3ee57ed01b9c63ff6790794aa5af4b5
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-11-27 14:50:59 +0000

    Updated build script with correct paths and folder names.

--- buildandpackage.sh
+++ buildandpackage.sh
@@ -2,8 +2,8 @@ cd Code
 rm -rf Site
 mkdir Site
 dotnet publish -o Site
-cp -r .platform/ Site
+cp -r ../.platform/ Site
 cp ../appspec.yml Site
-cp -r ../Scripts/ Site
+cp -r ../CodeDeployScripts/ Site
 cp ../ImageColourSwap.service Site
 cp ../ImageColourSwap.conf Site
