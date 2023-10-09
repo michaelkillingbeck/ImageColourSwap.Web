d9f95ce9f159541aed51c0f3a62f98bcf42f35f7
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2023-10-09 13:01:39 +0100

    Edited hosted zone name to add ics record to.

--- CodeDeployScripts/ApplicationRun.sh
+++ CodeDeployScripts/ApplicationRun.sh
@@ -7,7 +7,7 @@ export PATH=/home/ec2-user/.dotnet:$PATH
 export DOTNET_CLI_HOME=/tmp/
 
 output=$(curl -s https://checkip.amazonaws.com)
-hostedZoneID=$(aws route53 list-hosted-zones-by-name |  jq --arg name "michaelkillingbeck.com." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id')
+hostedZoneID=$(aws route53 list-hosted-zones-by-name |  jq --arg name "integration.michaelkillingbeck.com." -r '.HostedZones | .[] | select(.Name=="\($name)") | .Id')
 
 echo $(jq --arg output "$output" '.Changes[0].ResourceRecordSet.ResourceRecords[0].Value = $output' hosted-zone.json) > hosted-zone.json
 aws route53 change-resource-record-sets --hosted-zone-id $hostedZoneID --change-batch file://hosted-zone.json
