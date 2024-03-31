0db98fd109816e416f8d1930ef70105cb2ac2184
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2024-03-31 20:12:16 +0100

    Changed dotnet version install steps.

--- CodeDeployScripts/AfterInstall.sh
+++ CodeDeployScripts/AfterInstall.sh
@@ -4,7 +4,7 @@ touch output.txt
 
 sudo wget https://dot.net/v1/dotnet-install.sh >> output.txt
 sudo chmod +x ./dotnet-install.sh >> output.txt
-sudo ./dotnet-install.sh >> output.txt
+sudo ./dotnet-install.sh --version latest >> output.txt
 
 export PATH=/home/ec2-user/.dotnet:$PATH
 export DOTNET_CLI_HOME=/tmp/
